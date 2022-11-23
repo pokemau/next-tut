9592b8454a9d92328adcbd99edbcbec6b7143aec
pokemau <rentillosa90@gmail.com>
2022-11-23 17:47:01 +0800

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+      </Head>
+      <div className="about">
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam odio
+          incidunt tempore neque assumenda similique, iure explicabo asperiores
+          voluptatem amet commodi quae unde cumque! Numquam eligendi recusandae
+          libero voluptas inventore.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
